58a1a8bd9e4be72c565fd42f7c8e552903a19aa6
Игорь Климченков <klimchenkov@MacBook-Air-Igor.local>
2022-06-06 18:06:35 +0300

    calendar

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { privateRoutes, publicRoutes, RouteNames } from '../routes';
+
+const AppRouter = () => {
+    const {isAuth} = useTypedSelector( state => state.auth)
+    return (
+        isAuth 
+        ?
+        <Routes>
+            {privateRoutes.map(route =>
+                <Route path={route.path} 
+                       element={<route.component/>}
+                       key={route.path}
+                       />          
+            )}
+            <Route 
+                path='*'
+                element={<Navigate to={RouteNames.EVENT} replace />}/>
+        </Routes>
+        :
+        <Routes>
+            {publicRoutes.map(route =>
+                <Route path={route.path} 
+                    element={<route.component/>}
+                    key={route.path}
+                    />
+            )}
+            <Route 
+                path='*'
+                element={<Navigate to={RouteNames.LOGIN} replace />}/>
+        </Routes>
+    );
+};
+
+export default AppRouter;
